eaa9e8e8a6383ab90260e892f1891a3f6889e8e6
Nell Shamrell-Harrington <nells@microsoft.com>
2022-04-26 13:47:03 -0700

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+fn test_library_function() {
+    println!("I am a library function")
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
